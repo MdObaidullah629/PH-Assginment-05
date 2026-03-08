67078f725f1d90ed205452b7bc33aa4f4ce39dd6
MdObaidullah629 <hujaifaobaidullah629@gmail.com>
2026-03-09 02:05:23 +0600

    logine page succesfully complete with add js

--- js/login.js
+++ js/login.js
@@ -1,1 +1,18 @@
+function login(){
 
+const username = document.getElementById("username").value;
+const password = document.getElementById("password").value;
+
+if(username === "admin" && password === "admin123"){
+
+localStorage.setItem("isLoggedIn", true);
+
+window.location.href = "dashboard.html";
+
+}else{
+
+alert("Invalid Username or Password");
+
+}
+
+}
